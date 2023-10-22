b15fbaa1bd18708036886ac15af60c8d6074b047
Łukasz Grolik <lukasz@grolik.pl>
2023-10-22 20:15:32 +0200

    lukinfo search

--- src/lib/global-utils.tsx
+++ src/lib/global-utils.tsx
@@ -2,6 +2,7 @@ import * as Store from '../store/store';
 
 type SearchResult = {
     project: string;
+    chart: string | number;
     results: {
         line: number;
         code: string;
@@ -17,15 +18,35 @@ export class GlobalUtils {
 
     search(phrase: string): SearchResult[] {
         return this.store.projects.flatMap(project => {
-            return project.charts.flatMap(chart => {
+            return project.charts.flatMap((chart, chartIndex) => {
                 const lines = chart.code.toLowerCase().split('\n').flatMap((line, lineIndex) => {
-                    const index = line.indexOf(phrase.toLowerCase());
+                    // const index = line.indexOf(phrase.toLowerCase());
+                    const match = new RegExp(phrase).exec(line);
 
-                    return index > -1 ? [{ line: lineIndex + 1, code: line }] : [];
+                    return match ? [{ line: lineIndex + 1, code: line }] : [];
                 });
 
-                return lines.length ? [{ project: project.name, results: lines }] : [];
+                return lines.length ? [{ project: project.name, chart: chart.label || chartIndex + 1, results: lines }] : [];
             });
         });
+    }
+
+    lukinfoSearch(): string {
+        // defined here not to trigger vscode-lukinfo log
+        const sign = '!{1,3}';
+
+        return this.search(`// *${sign}`).map(chart => {
+            const results = chart.results.map(r => {
+                const m = r.code.match(/^\s*\/\/ *(?<sign>!{1,3}) +(?<msg>.+)$/)
+                if (!m) return '';
+
+                const sign = m.groups?.sign;
+                const message = m.groups?.msg;
+
+                return `  ${r.line}: ${sign} ${message}`;
+            }).join('\n');
+
+            return `${chart.project}/${chart.chart}\n${results}`;
+        }).join('\n');
     }
 }
